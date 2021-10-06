85775d8d4dbcae0c7ab2123f3a25a3de03642a7f
boterbaen <isabellesavage.ae@gmail.com>
2021-10-05 20:47:43 -0400

    messing with cameras

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DetectionTest.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.libs.AutoImport;
 
+@Disabled
 @Autonomous(name="DetectionTest", group="test")
 public class DetectionTest extends AutoImport {
 
@@ -15,8 +17,14 @@ public class DetectionTest extends AutoImport {
     public void runOpMode() {
         super.runOpMode();
 
-        if (opModeIsActive()) {
-            sleep(30000);
+        camera.startDetection();
+        dashboard.startCameraStream(camera.getWebCamera(), 0);
+
+        while (opModeIsActive()) {
+            packet.put("status", camera.getDetecting());
+            packet.put("detected", camera.getDetection());
+            packet.put("analysis", camera.getAnalysis());
+            dashboard.sendTelemetryPacket(packet);
         }
     }
 }
